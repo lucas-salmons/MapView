7f89ec3380d9ebc0de55484ad8b1ec8b24d0de0c
Lucas Salmons <lucsalmons@gmail.com>
2019-10-15 19:29:10 -0700

    seperated scripts

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var map = L.map("map", {
+  center: [47.608013, -122.335167],
+  zoom: 13
+});
+
+L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+//drives the navigation input value is split to a string as leaflet panTo requires an array
+function zoomTo() {
+  var inputLocation = document.getElementById("latLng").value;
+  var locationArray = inputLocation.split(",");
+  console.log(locationArray);
+  map.panTo(new L.latLng(locationArray));
+}
